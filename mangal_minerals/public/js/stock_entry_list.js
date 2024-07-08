27405d44fef9b4eb90015168405d43326f1b476b
krishnaArkay <krishna@arkayapps.com>
2024-07-08 12:19:55 +0000

    Stock Entry List View

--- mangal_minerals/public/js/stock_entry_list.js
+++ mangal_minerals/public/js/stock_entry_list.js
@@ -1,6 +1,13 @@
 frappe.listview_settings['Stock Entry'] = {
     onload: function(listview) {
-        if(!frappe.user.has_role('System Manager')){
+        if (!frappe.user.has_role('System Manager')) {
+            console.log("avyu onload")
+            frappe.set_route('mangal-minerals');
+        }
+    },
+    refresh: function(listview) {
+        if (!frappe.user.has_role('System Manager')) {
+            console.log("avyu refresh")
             frappe.set_route('mangal-minerals');
         }
     }
